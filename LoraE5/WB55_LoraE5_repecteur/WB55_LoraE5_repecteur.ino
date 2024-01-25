1e4ecefba1759582cabc3f7db770dd26bb61fd75
Weedmanu <terraspi13@gmail.com>
2024-01-25 08:29:30 +0100

    Update WB55_LoraE5_repecteur.ino

--- LoraE5/WB55_LoraE5_repecteur/WB55_LoraE5_repecteur.ino
+++ LoraE5/WB55_LoraE5_repecteur/WB55_LoraE5_repecteur.ino
@@ -1,43 +1,49 @@
-#include <Arduino.h>
-#include<SoftwareSerial.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include "logo.h"                           // logos fait pour OLED 128x64 
+/***** RECEPTEUR (STM32WB55 LORAE5 OLED128x64) *****/
+                      /****** Ajout des librairies nécessaires ******/
+#include<SoftwareSerial.h>         // pour ouvrir une deuxième com série
+#include <Wire.h>                  // pour l'I2C
+#include <Adafruit_GFX.h>          // pour l'écran oled
+#include <Adafruit_SSD1306.h>      // pour l'écran oled
+#include "logo.h"                  // les logos (fait pour OLED 128x64) 
 
-#define SCREEN_WIDTH    128                 // OLED largeur en pixel
-#define SCREEN_HEIGHT   64                  // OLED hauteur en pixel
-#define OLED_RESET      -1                  // GPIO ou -1 si vous partagez la broche de réinitialisation Arduino)
-
-// on déclare notre écran OLED
+                      /****** Déclaration des constantes, variables et objets ******/
+// pour notre écran OLED
+#define SCREEN_WIDTH    128        // OLED largeur en pixel
+#define SCREEN_HEIGHT   64         // OLED hauteur en pixel
+#define OLED_RESET      -1         // GPIO ou -1 si vous partagez la broche de réinitialisation Arduino)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-SoftwareSerial LoraE5(A0, A1);              // on ouvre une 2ème com série sur A0 A1 (on y branche le loraE RX TX)
-String inputString, message = "";           // pour la réception global et pour l'extraction du message
-bool stringComplete = false;                // un flag pour savoir quand le message reçu est complet
+// pour communiquer avec entre la WB55 et le LoraE5
+SoftwareSerial LoraE5(A0, A1);       // on ouvre une 2ème com série sur A0 A1 (on y branche le loraE RX TX)
+String inputString, message = "";    // pour la réception global et pour l'extraction du message
+bool stringComplete = false;         // un flag pour savoir quand le message reçu est complet
 
+                      /****** Déclaration des fonctions ******/
 // fonction qui efface l'écran et l'affiche vide
 void clear_display() {
-  display.clearDisplay();
-  display.display();  
+  display.clearDisplay();            // on efface l'écran
+  display.display();                 // on affiche l'écran vide
 }
 
 // fonction qui affiche le logo coeur à l'écran
 void love(void) {
-  display.clearDisplay(); // On efface l'écran   
+  display.clearDisplay();             // on efface l'écran   
+  // on dessine le logo (on calcul pour centrer sur x)
   display.drawBitmap((SCREEN_WIDTH-LOGO_COEUR_WIDTH)/2, 0, COEUR, LOGO_COEUR_WIDTH, LOGO_COEUR_HEIGHT, WHITE); 
-  display.display();
+  display.display();                  // on affiche le logo à l'écran
 }
 
 // fonction qui affiche le logo st choisi à l'écran 5s puis affiche le logo coeur 
 void logo_st(int ID) {
-  display.clearDisplay(); // On efface l'écran 
+  display.clearDisplay();             // on efface l'écran 
+  // si ID vaut 0 on dessine le logo 0 
   if (ID == 0) { display.drawBitmap((SCREEN_WIDTH-LOGO_ST_WIDTH)/2, 0, LOGO_ST, LOGO_ST_WIDTH, LOGO_ST_HEIGHT, WHITE);}
+  // sinon (ID vaut 1) on dessine le logo 1 
   else         {display.drawBitmap(0, 0, LOGO_STM32, LOGO_STM32_WIDTH, LOGO_STM32_HEIGHT, WHITE);}
-  display.display();
-  delay(5000);
-  clear_display();
-  love();
+  display.display();                  // on affiche le logo à l'écran
+  delay(5000);                        // on attend 5 secondes
+  clear_display();                    // on efface l'écran
+  love();                             // on lance la fonction love qui affiche le logo coeur à l'écran
 }
 
 // fonction qui affiche les datas de la DHT à l'écran 5s puis affiche le logo coeur
@@ -45,11 +51,10 @@ void display_dht(String temperature, String humidity) {
   display.clearDisplay();
   display.setTextSize(2);      
   display.setTextColor(SSD1306_WHITE); 
-  display.cp437(true);         // Use full 256 char 'Code Page 437' font
   display.setCursor(0, 0);
   display.print(temperature);
   display.print(" ");
-  display.write(248);
+  display.write(247);                  // symbole °   
   display.println("C");
   display.print(humidity);
   display.println(" %");
@@ -58,63 +63,39 @@ void display_dht(String temperature, String humidity) {
   clear_display();
   love();
 }
-/*
-// fonction qui sert a envoyer des commandes AT au module Lora
-// et retourne la reponse du module
-void comande_AT_LoraE5(int timeout_ms, char *commande) {
-  static char recv_buf[512];
-  static int caractere;
-  static int index = 0;
-  static int startMillis = 0;
-  memset(recv_buf, 0, sizeof(recv_buf));
-  LoraE5.print(commande);
-  Serial.print(commande);
-  delay(100);
-  startMillis = millis();
-  do {
-    while (LoraE5.available() > 0) {
-      caractere = LoraE5.read();
-      recv_buf[index++] = caractere;
-      Serial.print((char)caractere);
-      delay(2);
-    }
-  } while (millis() - startMillis < timeout_ms);
-  Serial.println();
-}*/
 
-// fonction qui sert a envoyer des commandes AT au module Lora
-// et retourne la reponse du module
-void comande_AT_LoraE5(int timeout_ms, char *commande) {
-  static char recv_buf[512];
-  static int caractere;
-  static int index = 0;
-  static int startMillis = 0;
-  memset(recv_buf, 0, sizeof(recv_buf));
-  LoraE5.print(commande);
-  Serial.print(commande);
-  delay(100);
-  while (LoraE5.available() > 0) {
-    caractere = LoraE5.read();
-    recv_buf[index++] = caractere;
-    Serial.print((char)caractere);
-    delay(2);
-    if ((char)caractere == '\n') {
-      delay(1000);
-      break;
+// fonction qui sert a envoyer des commandes AT au module LoraE5
+// et retourne la réponse du module
+void comande_AT_LoraE5(char *commande) {     
+  static char recv_buf[512];                 // on réserve un buffer pour stocker les bytes qui arrivent
+  static int caractere;                      // pour stocker chaque byte 
+  static int index = 0;                      // la place dans le buffer
+  memset(recv_buf, 0, sizeof(recv_buf));     // on stock en mémoire le buffer
+  LoraE5.print(commande);                    // on envoie la commande au module LoraE5 
+  Serial.print(commande);                    // on écrit la commande dans la com série avec le pc pour infos
+  delay(100);                                // un petit délais
+  while (LoraE5.available() > 0) {           // quand quelque chose arrive du LoraE5
+    caractere = LoraE5.read();               // on le place dans une variable 
+    recv_buf[index++] = caractere;           // on ajoute au buffer cette variable  
+    Serial.print((char)caractere);           // on écrit cette variable en char dans la com série avec le pc pour infos
+    delay(2);                                // un petit délais
+    if ((char)caractere == '\n') {           // si le caractère est un retour à la ligne
+      delay(1000);                           // un petit délais
+      break;                                 // on sort de la boucle
     }
   }
-  Serial.println();
+  Serial.println();                          // une ligne vide dans la com série avec le pc pour espacer
 }
 
 // fonction pour convertir une string en Hexa en string ASCII
 String hexToString(String hex){                   // un paramètre: le message en hex a convertir
   String text = "";                               // on déclare une string vide pour réceptionner les caractères convertis
   for(int k=0;k< hex.length();k++) {              // pour chaque caractère du message :
-    if(k%2!=0) {                                    // si la place de ce caractère dans le message est impaire
-      char temp[3];                                 // on créé un tableau vide de 3
-      sprintf(temp,"%c%c",hex[k-1],hex[k]);         // on y place le caractère précédent et ce caractère 
-      int number = (int)strtol(temp, NULL, 16);     // on converti cette partie de hexa en int    
-      text+=char(number);                           // on ajoute dans notre string de réception le char correspondant 
+    if(k%2!=0) {                                  // si la place de ce caractère dans le message est impaire
+      char temp[3];                               // on créé un tableau vide de 3
+      sprintf(temp,"%c%c",hex[k-1],hex[k]);       // on y place le caractère précédent et ce caractère 
+      int number = (int)strtol(temp, NULL, 16);   // on converti cette partie de hexa en int    
+      text+=char(number);                         // on ajoute dans notre string de réception le char correspondant 
     }  
   }  
   return text;                                    // on retourne notre string de réception
@@ -162,26 +143,26 @@ void E5serialEvent() {
   }
 }
 
-// Le setup
-void setup(void) {
+                      /****** setup ******/
+void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3c);                                      // initialise l'écran, adresse I2C : 0x3C 
-  display.clearDisplay();                                                         // On efface l'écran   
+  display.clearDisplay();                                                         // on efface l'écran   
   love();                                                                         // on lance la fonction qui affiche le logo cœur
   Serial.begin(9600);                                                             // on démarre la com série avec le pc à 9600 baud
   LoraE5.begin(9600);                                                             // on démarre la com série avec le module LoraE5 à 9600 baud
   Serial.println("LoraE5 config");                                                // on écrit dans la com série avec le pc pour infos
-  comande_AT_LoraE5(1000, "AT\r\n");                                              // on lance une commande de test avec notre fonction comande_AT_LoraE5
+  comande_AT_LoraE5("AT\r\n");                                                    // on lance une commande de test avec notre fonction comande_AT_LoraE5
   delay(200);                                                                     // un petit délais 
-  comande_AT_LoraE5(1000, "AT+MODE=TEST\r\n");                                    // on lance la commande pour entrer en mode test
+  comande_AT_LoraE5("AT+MODE=TEST\r\n");                                          // on lance la commande pour entrer en mode test
   delay(200);                                                                     // un petit délais 
   // on lance la commande de config: 
   // Fréquence, mode, bande passante, puissance RX, puissance TX, CRC (check nb de byte envoyés et reçus), IQ (inversé), réseau public/privé (off/on) 
-  comande_AT_LoraE5(2000, "AT+TEST=RFCFG,866,SF12,125,12,12,15,ON,OFF,OFF\r\n");  
-  comande_AT_LoraE5(2000, "AT+TEST=RXLRPKT\r\n");                                 // on lance la commande qui met le module en mode réception
-  Serial.print("LoraE5 en mode réception\r\n");                                   // on écrit dans la com série avec le pc pour infos
+  comande_AT_LoraE5("AT+TEST=RFCFG,866,SF12,125,12,12,15,ON,OFF,OFF\r\n");  
+  comande_AT_LoraE5("AT+TEST=RXLRPKT\r\n");                                       // on lance la commande qui met le module en mode réception
+  Serial.println("LoraE5 en mode réception");                                     // on écrit dans la com série avec le pc pour infos
 }
 
-// le loop
-void loop(void){
+                      /****** loop ******/
+void loop(){
   E5serialEvent();  // on écoute la com série du LoraE5 en boucle
 }
